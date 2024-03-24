e483621e8f3d4911fe2c0a37f82184459931e448
Ezeobiekene <ezeobiekene7@gmail.com>
2024-03-24 15:24:05 -0400

    add 2 answers

---
+++ codewars.js
@@ -0,0 +1,16 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord( bool ){
+    //...
+    if (bool) {
+      return "Yes";
+    } else {
+      return "No";
+    }
+  }
+
+
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function evenOrOdd(number) {
+    return number % 2 === 0 ? 'Even' : 'Odd';
+  }
