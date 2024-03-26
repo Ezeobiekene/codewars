ddf567d56ba51b015f8afb46c584ce0bf5d1c6bb
Ezeobiekene <ezeobiekene7@gmail.com>
2024-03-26 01:43:36 -0400

    solved Opposites Attract

--- codewars.js
+++ codewars.js
@@ -397,4 +397,17 @@ function squareDigits(num){
      
      return Number(res);
    }
+
+
+
+
+//    DESCRIPTION:
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
    
+function lovefunc(flower1, flower2){
+
+  //if the remainder of the flowers when divided by two are the same then the conditions are false
+  return flower1 % 2 === flower2 % 2 ? false : true
+}
