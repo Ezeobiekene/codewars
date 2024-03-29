4f376f0037a75a11ac52a60347b68810dcd8f6d7
Ezeobiekene <ezeobiekene7@gmail.com>
2024-03-29 19:52:28 -0400

    solved a question

--- codewars.js
+++ codewars.js
@@ -453,3 +453,16 @@ function sum (numbers) {
  //return the sum
  return sum
 };
+
+
+
+// Instructions
+// Make a simple function called greeet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+let greeet = function(){
+  return 'hello world!'
+}
