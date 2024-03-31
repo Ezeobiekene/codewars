ed3ee01a866b9fecbb7fac5aab418926c0be04c2
Ezeobiekene <ezeobiekene7@gmail.com>
2024-03-30 20:25:59 -0400

    solved question

--- codewars.js
+++ codewars.js
@@ -543,3 +543,19 @@ function oddOrEven(array) {
  
  return (sum % 2 === 0) ? 'even' : 'odd'
 }
+
+
+// DESCRIPTION:
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
