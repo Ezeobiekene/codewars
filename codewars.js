4498124773026a40989253abb03423cef9edae21
Ezeobiekene <ezeobiekene7@gmail.com>
2024-03-30 20:33:57 -0400

    solved question

--- codewars.js
+++ codewars.js
@@ -559,3 +559,27 @@ function oddOrEven(array) {
 function disemvowel(str) {
   return str.replace(/[aeiou]/gi, '');
 }
+
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+
+function countSheeps(sheep) {
+  // TODO
+  let sum = 0
+  for(let ele of sheep){
+    if (ele) sum += 1
+  }
+  
+  return sum
+}
