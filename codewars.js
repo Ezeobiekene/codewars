df96648f182eeff16a65fd372ccb59fc348d2ba0
Ezeobiekene <ezeobiekene7@gmail.com>
2024-04-03 22:03:06 -0400

    solved an additional question

--- codewars.js
+++ codewars.js
@@ -688,3 +688,26 @@ function reverseWords(str) {
   
   return res.trim()
 }
+
+
+
+// Instructions
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+
+function areYouPlayingBanjo(name) {
+  let res = ''
+   
+  if(name[0] === 'r' || name[0] === 'R' ) {
+    return name + " plays banjo" 
+  }
+  
+   return name + " does not play banjo"
+ }
