60158f460e5534e78cb3806633dc6b5bc401ee6c
Ezeobiekene <ezeobiekene7@gmail.com>
2024-04-08 02:30:39 -0400

    solved a question

--- codewars.js
+++ codewars.js
@@ -754,3 +754,13 @@ function positiveSum(arr) {
     return acc;
   }, 0)
 }
+
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  return str.split('').reverse().join('')
+}
