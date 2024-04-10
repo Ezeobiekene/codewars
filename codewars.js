c954707c06aea0fc60055471b9c0c42f2e47344d
Ezeobiekene <ezeobiekene7@gmail.com>
2024-04-09 22:16:54 -0400

    solved a question

--- codewars.js
+++ codewars.js
@@ -764,3 +764,34 @@ function positiveSum(arr) {
 function solution(str){
   return str.split('').reverse().join('')
 }
+
+
+
+
+
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+
+
+
+
+
+function sumArray(array) {
+
+  if(!array) return 0
+  array = array.sort((a, b) => a - b)
+  let sum = 0
+  for(let i = 1; i < array.length - 1; i++){
+    sum += array[i]
+  }
+  
+  return sum
+}
